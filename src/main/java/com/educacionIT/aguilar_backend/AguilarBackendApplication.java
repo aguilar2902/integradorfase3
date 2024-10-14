615a349a837316254a4123d27ca43e1357a2645a
Jonathan <aguilarjonathan2902@gmail.com>
2024-10-14 10:33:51 -0300

    Creacion del proyecto Maven, incluye la configuracion inicial

---
+++ src/main/java/com/educacionIT/aguilar_backend/AguilarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.educacionIT.aguilar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguilarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguilarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educacionIT/aguilar_backend/AguilarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacionIT.aguilar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguilarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
